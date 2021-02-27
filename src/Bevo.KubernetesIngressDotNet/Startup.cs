d5385a6ffd2f56105ec925415a8b028c2e3542d4
David Bevin <dpbevin@gmail.com>
2021-02-28 08:40:55 +1100

    Cleanup application code.

--- src/Bevo.KubernetesIngressDotNet/Startup.cs
+++ src/Bevo.KubernetesIngressDotNet/Startup.cs
@@ -1,14 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.ReverseProxy.Abstractions.Config;
 
 namespace KubernetesIngressDotNet
 {
@@ -21,21 +15,12 @@ namespace KubernetesIngressDotNet
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddReverseProxy()
-                .LoadFromKubernetes(this.Configuration.GetSection("KubeRP"))
-                .AddTransforms(builderContext =>
-                {
-                    // Added to all routes.
-                    builderContext.AddRequestHeader(headerName: "MyHeader", value: "MyValue", append: true);
-                });
-                //.LoadFromConfig(this.Configuration.GetSection("ReverseProxy"));
+                .LoadFromKubernetes(this.Configuration.GetSection("KubeRP"));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
